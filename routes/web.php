b65977d3375fce9111dff739a3c9467e6a8115cd
Ubuntu <ubuntu@ip-172-31-37-66.us-east-2.compute.internal>
2026-02-07 14:45:55 +0000

    Remove auth redirect routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,3 @@
-// Redirect /login and related auth routes to auth-system login page
-Route::get('/login', function () {
-    return redirect()->away('http://auth-system.test/login?return_url=http://lunarblood.test');
-});
-Route::get('/register', function () {
-    return redirect()->away('http://auth-system.test/register?return_url=http://lunarblood.test');
-});
-Route::get('/forgot-password', function () {
-    return redirect()->away('http://auth-system.test/forgot-password?return_url=http://lunarblood.test');
-});
 <?php
 
 use Illuminate\Support\Facades\Route;
