7eeb183c0d12abb9d5a94d3120afa6c26d465a7d
joshua <ackerlyj05@gmail.com>
2026-07-02 02:44:20 -0400

    fix(lint): use eslint-disable for import.meta.glob<any> (required by laravel-vite-plugin)

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,7 +2,6 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { type ComponentType } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ToastProvider } from './components/Toast';
 import { initializeTheme } from './hooks/use-appearance';
@@ -13,7 +12,8 @@ const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID as string | undef
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob<{ default: ComponentType }>('./pages/**/*.tsx')),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob<any>('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
